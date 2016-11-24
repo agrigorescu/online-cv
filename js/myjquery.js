0d86469c02104f031edf123096a23a2bdf6cc956
andreea grigorescu <deea26@gmail.com>
2016-11-24 16:34:21 +0000

    changed the navbar and made it show and the links work however it does not display in a list grrr

---
+++ js/myjquery.js
@@ -0,0 +1,10 @@
+$(function(){
+    console.log("the dom is ready");
+
+    //collapsing the navbar when in mobile mode
+    $(".button-collapse").sideNav();
+
+    //including the parallax scrolling effect
+    $('.parallax').parallax();
+
+})
--- myjs/myjquery.js
+++ myjs/myjquery.js
@@ -2,9 +2,11 @@ $(function(){
     console.log("the dom is ready");
 
     //collapsing the navbar when in mobile mode
-    $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav({
+      closeOnClick: true;
+    });
 
     //including the parallax scrolling effect
     $('.parallax').parallax();
 
-});
+})
