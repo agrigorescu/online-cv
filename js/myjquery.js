0f13da1182bc2480aa7ef5114389694a3ee7a73a
andreea grigorescu <deea26@gmail.com>
2016-11-27 16:50:30 +0000

    made extra pages for details from jobs and school. the buttons for the school are not centered properly. needs investigating

--- js/myjquery.js
+++ js/myjquery.js
@@ -33,5 +33,9 @@ $(function(){
         startDelay: 5000
       });
 
+      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      // $('.modal').modal();
+      $("#demo01").animatedModal();
+
 
 });
